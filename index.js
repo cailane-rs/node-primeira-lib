ceba5b759fb3096fd64a2f85f22800e1b4c153c9
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-06-18 14:58:25 -0300

    function pegaArquivo com promises e then()

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if(erro) {
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    })
-}
+// versão assíncrona: promises com then()
+ function pegaArquivo(caminhoDoArquivo) {
+     const encoding = 'utf-8';
+     fs.promises.readFile(caminhoDoArquivo, encoding)
+     .then((texto) => console.log(chalk.green(texto)))
+     .catch((erro) => trataErro(erro))
+ }
+
+// versão síncrona
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
 
 pegaArquivo('./arquivos/texto.md');
